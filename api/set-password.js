1f4c44864218cbe9de2775f9ce2a331636e8ce1d
Ridwan0126 <muhamadridwan6792@gmail.com>
2026-06-19 11:07:41 +0700

    CM24

--- api/set-password.js
+++ api/set-password.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
         Referer: "https://rpp.bapenda.jatengprov.go.id/",
         Origin: "https://rpp.bapenda.jatengprov.go.id/",
+        Host: "rpp.bapenda.jatengprov.go.id",
       },
     };
 
