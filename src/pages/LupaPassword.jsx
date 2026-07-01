610cf5b064c46e66bc38bc82ae6aa59aa191f821
Ridwan0126 <muhamadridwan6792@gmail.com>
2026-07-01 16:05:51 +0700

    cm15

--- src/pages/LupaPassword.jsx
+++ src/pages/LupaPassword.jsx
@@ -59,8 +59,7 @@ export default function LupaPassword() {
       if (response.ok || data.code === "00") {
         setStatus("success");
         setMessage(
-          data.message ||
-            "Email reset password telah dikirim ke email Anda!, dan cek folder SPAM jika tidak masuk.",
+          "Email reset password telah dikirim ke email Anda!, dan cek folder SPAM jika tidak masuk.",
         );
         setFormData({ nik_npwp: "", email_rpp: "" });
       } else {
