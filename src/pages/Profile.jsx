7a09c88d6b19356a7f6ea78b2100c0b95b4ece72
Ridwan0126 <muhamadridwan6792@gmail.com>
2026-07-01 15:13:34 +0700

    cm06

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -157,7 +157,7 @@ export default function Profile() {
                     </div>
 
                     <div className="bg-green-500/10 border border-green-500/20 text-green-300 px-4 py-2 rounded-xl">
-                      Aktif
+                      <a href="/lupapassword">Lupa Password</a>
                     </div>
 
                     <div className="bg-purple-500/10 border border-purple-500/20 text-purple-300 px-4 py-2 rounded-xl">
